291cd52e2e576e3062b505a81b9b2c35ea908420
Rayala07 <rayala07viswanath@Gmail.com>
2026-02-13 15:55:46 +0530

    Create post and store in DB

--- Backend/src/controllers/post.controller.js
+++ Backend/src/controllers/post.controller.js
@@ -1,5 +1,7 @@
 const imageKit = require("@imagekit/nodejs");
 const { toFile } = require("@imagekit/nodejs");
+const postModel = require("../models/post.model");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const client = new imageKit({
@@ -7,13 +9,39 @@ const client = new imageKit({
 });
 
 async function createPostController(req, res) {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({
+      message: "Unauthorized",
+    });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (err) {
+    return res.status(401).json({
+      message: "User not authorized",
+    });
+  }
+
   // File upload to imagekit
   const file = await client.files.upload({
     file: await toFile(Buffer.from(req.file.buffer), "file"),
     fileName: "image1",
+    folder: "feedlyx-posts",
   });
 
-  res.send(file);
+  const post = await postModel.create({
+    caption: req.body.caption,
+    img_url: file.url,
+    user: decoded.id,
+  });
+
+  res.status(201).json({
+    message: "Post created",
+    post,
+  });
 }
 
 module.exports = {
