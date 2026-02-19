4e05684a9f33f87ed98d5d616868237aa7bdf860
Rayala07 <rayala07viswanath@Gmail.com>
2026-02-19 17:50:56 +0530

    add: followers-schema-model

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -20,6 +20,19 @@ const userSchema = new mongoose.Schema({
     type: String,
     default: "https://ik.imagekit.io/htg6mf2k3/defaultimage.jpg",
   },
+  // Not possible to store followers and following as array of ObjectIds because of the limit of 100 items in an array in mongoose, so we will create a separate collection for followers and following, this concept is called as "Edge-collection".
+  // followers: [
+  //   {
+  //     type: mongoose.Schema.Types.ObjectId,
+  //     ref: "Users",
+  //   },
+  // ],
+  // following: [
+  //   {
+  //     type: mongoose.Schema.Types.ObjectId,
+  //     ref: "Users",
+  //   },
+  // ],
 });
 
 const userModel = mongoose.model("Users", userSchema);
